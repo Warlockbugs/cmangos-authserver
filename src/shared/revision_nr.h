Warlockbugs/cmangos-authserver

commit aa36f2219326688b9802580833ca82c187311a42

[s0361] Convert many opcode handlers to ObjectGuid use.
Also re-enabled anticheating check for bank window open
base at .bank command access rights.

(based on commit b8d2536)

src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "0360"
+ #define REVISION_NR "0361"
 #endif // __REVISION_NR_H__

commit 9a6e9e8b2e18dfb4121e3ae6cce370b8f6c98cfc

[s1338] Fix unlearning talents of hunter pets
This was caused by a typo in aa36f22193

src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "1337"
+ #define REVISION_NR "1338"
 #endif // __REVISION_NR_H__